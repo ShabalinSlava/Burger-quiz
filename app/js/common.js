c6eb6a0fb004aa09b267a26a29dce3c9207a0313
Shabalin Slava <svaznoy0309@gmail.com>
2020-05-17 13:00:06 +0800

    added event prev and next buttons

--- app/js/common.js
+++ app/js/common.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function(){
   const formAnswers = document.querySelector('#formAnswers');
   const nextButton = document.querySelector('#next');
   const prevButton = document.querySelector('#prev');
+  const sendButton = document.querySelector('#send');
+  // Массив вопросы и ответы
   const questions = [
     {
       question: "Какого цвета бургер?",
@@ -90,16 +92,18 @@ document.addEventListener('DOMContentLoaded', function(){
   closeModal.addEventListener('click', () => {
     modalBlock.classList.remove('d-block');
   });
-  // Тест
+  // Начало тестирования
   const playTest = () => {
+    const finalAnswers = [];
+    // Переменная с номером вопроса
     let numberQuestion = 0;
-    // Создание элемента через цикл
+    // Функция рендеринга ответов
     const renderAnswers = (index) => {
       questions[index].answers.forEach((answer) => {
         const answerItem = document.createElement('div');
-        answerItem.classList.add('answers-item', 'd-flex', 'flex-column')
+        answerItem.classList.add('answers-item', 'd-flex', 'justify-content-center')
         answerItem.innerHTML = `
-        <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+        <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}">
         <label for="${answer.title}" class="d-flex flex-column justify-content-between">
           <img class="answerImg" src="${answer.url}" alt="burger">
           <span>${answer.title}</span>
@@ -107,19 +111,68 @@ document.addEventListener('DOMContentLoaded', function(){
         `;
         formAnswers.appendChild(answerItem);
       })
-    }
+    };
+    // Функция рендеринга вопросов и ответов
     const renderQuestions = (indexQuestion) => {
       formAnswers.innerHTML = '';
-      questionTitle.textContent = `${questions[indexQuestion].question}`;
-      renderAnswers(indexQuestion);
+      if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+        questionTitle.textContent = `${questions[indexQuestion].question}`;
+        renderAnswers(indexQuestion);
+        nextButton.classList.remove('d-none');
+        prevButton.classList.remove('d-none');
+        sendButton.classList.add('d-none');
+      };
+      if (numberQuestion === 0) {
+        prevButton.classList.add('d-none');
+      };
+      if (numberQuestion === questions.length) {
+        nextButton.classList.add('d-none');
+        prevButton.classList.add('d-none');
+        sendButton.classList.remove('d-none');
+        formAnswers.innerHTML = `
+        <div class='form-group'>
+          <label for='numberPhone'>Enter your number</label>
+          <input type='phone' class='form-control' id='numberPhone' />
+        </div>
+        `;
+      };
+        if (numberQuestion === questions.length + 1) {
+          formAnswers.textContent = 'Спасибо за пройденный тест!';
+          setTimeout(() => {
+            modalBlock.classList.remove('d-block');
+          }, 2000);
+        }
     };
+    // Запуск функции рендеринга
     renderQuestions(numberQuestion);
+
+    const checkAnswer = () => {
+      const obj = {};
+      const inputs = [...formAnswers.elements].filter((input) => input.checked || input.id === 'numberPhone');
+      inputs.forEach((input, index) => {
+        if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+          obj[`${index}_${questions[numberQuestion].question}`] = input.value;
+        }
+        if (numberQuestion === questions.length) {
+          obj['Номер телефона'] = input.value;
+        }
+      });
+      finalAnswers.push(obj);
+    };
+
+    // Обработчики событий кнопок next and prev
     nextButton.onclick = () => {
+      checkAnswer();
       numberQuestion++;
       renderQuestions(numberQuestion);
     };
     prevButton.onclick = () => {
       numberQuestion--;
+      renderQuestions(numberQuestion);
+    };
+    sendButton.onclick = () => {
+      checkAnswer();
+      numberQuestion++;
       renderQuestions(numberQuestion);
     };
   };
